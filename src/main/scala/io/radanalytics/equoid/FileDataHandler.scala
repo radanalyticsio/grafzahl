68aeec7bb08056c9f81720a8d087ed8a43cecf8e
Jirka Kremser <jkremser@redhat.com>
2018-04-05 14:02:44 +0200

    getProp now accepts the name of the property in the SNAKE_CASE, hopefuly it's less misleading now

--- src/main/scala/io/radanalytics/equoid/FileDataHandler.scala
+++ src/main/scala/io/radanalytics/equoid/FileDataHandler.scala
@@ -2,31 +2,13 @@ package io.radanalytics.equoid
 
 import java.lang.Long
 
-import org.apache.log4j.{Level, LogManager, PropertyConfigurator, Logger}
-
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
-import org.apache.spark.storage.StorageLevel
-import org.apache.spark.rdd
-import org.apache.spark.streaming.amqp.AMQPUtils
-import org.apache.spark.streaming.{Duration, Seconds, StreamingContext}
-
-import org.apache.spark.util.sketch.CountMinSketch
-import io.radanalytics.equoid._
-import org.apache.spark.sql.SparkSession
-
-import org.apache.spark.util.sketch
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.util.LongAccumulator
-import scala.util.Random
-import scala.util.Properties
-
-import org.infinispan._
-import org.infinispan.client.hotrod.RemoteCache
 import org.infinispan.client.hotrod.RemoteCacheManager
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder
-import org.infinispan.client.hotrod.impl.ConfigurationProperties
 
-import scala.collection.immutable
+import scala.util.Properties
 
 object FileIntervalAccumulator {
 
@@ -81,11 +63,11 @@ object FileDataHandler {
   }
 
   def createStreamingContext(): StreamingContext = {
-    val infinispanHost = getProp("jdgHost", "localhost")
-    val infinispanPort = getProp("jdgPort", "11222").toInt
-    val k = getProp("cmsK", "3").toInt
-    val epsilon = getProp("cmsEpsilon", "0.01").toDouble
-    val confidence = getProp("cmsConfidence", "0.9").toDouble    
+    val infinispanHost = getProp("JDG_HOST", "localhost")
+    val infinispanPort = getProp("JDG_PORT", "11222").toInt
+    val k = getProp("CMS_K", "3").toInt
+    val epsilon = getProp("CMS_EPSILON", "0.01").toDouble
+    val confidence = getProp("CMS_CONFIDENCE", "0.9").toDouble
     val conf = new SparkConf().setMaster(master).setAppName(appName)
     conf.set("spark.streaming.receiver.writeAheadLog.enable", "true")
     
